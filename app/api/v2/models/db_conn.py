5ce761684567efe8e8b616411a2dd46a31d5293b
Arrotech <arrotechdesign@gmail.com>
2019-02-13 17:58:58 -0500

    chore(Database connection) Create database connection - create a config file - create cli commands [Finishes #163965248]

---
+++ app/api/v2/models/db_conn.py
@@ -0,0 +1,16 @@
+import psycopg2
+import os
+from psycopg2.extras import RealDictCursor
+
+
+class Database:
+    """Initialization."""
+
+    def __init__(self):
+        self.db_name = os.getenv('DB_NAME')
+        self.db_host = os.getenv('DB_HOST')
+        self.db_user = os.getenv('DB_USER')
+        self.db_password = os.getenv('DB_PASSWORD')
+        self.conn = psycopg2.connect(
+            database=self.db_name, host=self.db_host, user=self.db_user, password=self.db_password)
+        self.curr = self.conn.cursor(cursor_factory=RealDictCursor)
--- run.py
+++ run.py
@@ -1,7 +1,20 @@
 from app import electoral_app
 from app.api.v2.models.db_conn import Database
+import os
 
-app = electoral_app()
+config_name = os.getenv('APP_SETTINGS')  # config_name = "development"
+app = electoral_app(config_name)
+
+
+@app.cli.command()
+def create():
+    Database().create_table()
+
+
+@app.cli.command()
+def destroy():
+    Database().destroy_table()
+
 
 if __name__ == "__main__":
     app.run(debug=True)
